8bea81f440c08774a6a6a603b77a91029e08eb72
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-10-29 15:05:25 -0300

    Feat: Create User Request

---
+++ src/main/java/com/jotace/picpay/dto/UserRequest.java
@@ -0,0 +1,24 @@
+package com.jotace.picpay.dto;
+
+import com.jotace.picpay.domain.user.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record UserRequest(
+        @NotNull
+        String fullName,
+        @NotNull
+        String cpf,
+        @Email
+        @NotNull
+        String email,
+        @NotNull
+        String password,
+        UserType userType,
+        @NotNull
+        BigDecimal amount
+
+) {
+}
