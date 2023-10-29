a65cb5c48a260ac8994b8172df95736a095036bc
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-10-29 13:15:04 -0300

    Feat: Create entity users

---
+++ src/main/java/com/jotace/picpay/domain/User.java
@@ -0,0 +1,29 @@
+package com.jotace.picpay.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "users")
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private UserType userType;
+}
---
+++ src/main/java/com/jotace/picpay/domain/UserType.java
@@ -0,0 +1,6 @@
+package com.jotace.picpay.domain;
+
+public enum UserType {
+    MERCHANT,
+    COMMON
+}
