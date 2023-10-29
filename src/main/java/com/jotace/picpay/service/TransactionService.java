8503815d59f9da5a3b00789d72ebc527c6d055de
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-10-29 15:08:23 -0300

    Feat: configuring the method createTransaction

--- src/main/java/com/jotace/picpay/service/TransactionService.java
+++ src/main/java/com/jotace/picpay/service/TransactionService.java
@@ -1,12 +1,15 @@
 package com.jotace.picpay.service;
 
 import com.jotace.picpay.domain.transaction.Transaction;
-import com.jotace.picpay.domain.user.User;
+import com.jotace.picpay.dto.TransactionRequest;
+import com.jotace.picpay.dto.TransactionResponse;
 import com.jotace.picpay.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
+import java.time.LocalDate;
 
+@Service
 public class TransactionService {
     @Autowired
     private UserService service;
@@ -14,7 +17,27 @@ public class TransactionService {
     @Autowired
     private TransactionRepository repository;
 
-    public Transaction createTransaction(User sender, BigDecimal value) {
+    public TransactionResponse createTransaction(TransactionRequest request) {
+        var sender = service.findUserById(request.senderId());
+        var receiver = service.findUserById(request.receiverId());
+        var value = request.value();
 
+        service.validate(sender, value);
+
+        sender.setAmount(sender.getAmount().subtract(value));
+        receiver.setAmount(sender.getAmount().add(value));
+
+        var transaction = new Transaction();
+
+        transaction.setSender(sender);
+        transaction.setReceiver(receiver);
+        transaction.setTransactionValue(value);
+        transaction.setDate(LocalDate.now());
+
+        service.save(sender);
+        service.save(receiver);
+        repository.save(transaction);
+
+        return new TransactionResponse(transaction);
     }
 }
