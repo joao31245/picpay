fadaa39764a9e8e1bc6246104fc5ec4fe2255055
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-10-30 18:53:27 -0300

    Feat: Add the code to send e-mails.

--- src/main/java/com/jotace/picpay/infra/ses/SesEmailSender.java
+++ src/main/java/com/jotace/picpay/infra/ses/SesEmailSender.java
@@ -1,9 +1,9 @@
 package com.jotace.picpay.infra.ses;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.jotace.picpay.adapters.EmailSenderGateway;
-import com.jotace.picpay.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +21,15 @@ public class SesEmailSender implements EmailSenderGateway {
     @Override
     public void sendEmail(String to, String subject, String body) {
        var request = new SendEmailRequest()
-               .withSource("")
+               .withSource("trafalgardjaohearth@gmail.com")
                .withDestination(new Destination().withToAddresses(to))
                .withMessage(new Message()
                        .withSubject(new Content(subject))
                        .withBody(new Body().withText(new Content(body))));
-
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch(AmazonServiceException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
