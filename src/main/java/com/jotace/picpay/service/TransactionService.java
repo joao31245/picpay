a03471267d45d874a7f7e5dbb17a368ce645346f
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-10-30 17:40:05 -0300

    Feat: add notifications

--- src/main/java/com/jotace/picpay/service/TransactionService.java
+++ src/main/java/com/jotace/picpay/service/TransactionService.java
@@ -2,7 +2,6 @@ package com.jotace.picpay.service;
 
 import com.jotace.picpay.domain.transaction.Transaction;
 import com.jotace.picpay.dto.TransactionRequest;
-import com.jotace.picpay.dto.TransactionResponse;
 import com.jotace.picpay.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,17 +11,23 @@ import java.time.LocalDate;
 @Service
 public class TransactionService {
     @Autowired
-    private UserService service;
+    private UserService userService;
 
     @Autowired
     private TransactionRepository repository;
+    private final NotificationService notificationService;
+
+    @Autowired
+    public TransactionService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
     public Transaction createTransaction(TransactionRequest request) {
-        var sender = service.findUserById(request.senderId());
-        var receiver = service.findUserById(request.receiverId());
+        var sender = userService.findUserById(request.senderId());
+        var receiver = userService.findUserById(request.receiverId());
         var value = request.value();
 
-        service.validate(sender, value);
+        userService.validate(sender, value);
 
         sender.setAmount(sender.getAmount().subtract(value));
         receiver.setAmount(receiver.getAmount().add(value));
@@ -34,9 +39,15 @@ public class TransactionService {
         transaction.setTransactionValue(value);
         transaction.setDate(LocalDate.now());
 
-        service.save(sender);
-        service.save(receiver);
+        userService.save(sender);
+        userService.save(receiver);
         repository.save(transaction);
+
+        notificationService.sendEmail(sender.getEmail(),"Money transaction", "You sent "
+                + transaction.getTransactionValue() + " to " + receiver.getFullName());
+
+        notificationService.sendEmail(receiver.getEmail(),"Money transaction", "You received "
+                + transaction.getTransactionValue() + " from " + sender.getFullName());
 
         return transaction;
     }
