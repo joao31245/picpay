f03341360195b07696e38457a6f330e07f40fd77
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-11-06 21:25:37 -0300

    Feat: Documenting code

--- src/main/java/com/jotace/picpay/PicpayApplication.java
+++ src/main/java/com/jotace/picpay/PicpayApplication.java
@@ -1,9 +1,14 @@
 package com.jotace.picpay;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "Pic Pay simplified", version = "1", description = "Api that makes transactions"))
 public class PicpayApplication {
 
 	public static void main(String[] args) {
