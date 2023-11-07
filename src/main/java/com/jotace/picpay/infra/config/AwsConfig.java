e50faae51c9a837d6f6ee5f60c80ceaa550107ee
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-11-06 21:24:49 -0300

    Feat: Add environment variables at the code.

--- src/main/java/com/jotace/picpay/infra/config/AwsConfig.java
+++ src/main/java/com/jotace/picpay/infra/config/AwsConfig.java
@@ -1,16 +1,28 @@
 package com.jotace.picpay.infra.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfig {
+
+    @Value("${aws.accessKeyId}")
+    String accessKey;
+
+    @Value("${aws.secretKey}")
+    String secretKey;
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(Regions.US_EAST_1)
                 .build();
     }
