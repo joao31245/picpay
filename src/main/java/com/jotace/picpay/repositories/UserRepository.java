e46b58f707c1db3448ad9de8573d5d49867c395e
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-11-17 23:18:37 -0300

    Feat: Add new queries

--- src/main/java/com/jotace/picpay/repositories/UserRepository.java
+++ src/main/java/com/jotace/picpay/repositories/UserRepository.java
@@ -3,6 +3,11 @@ package com.jotace.picpay.repositories;
 import com.jotace.picpay.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserById(Long id);
+
+    User findUserByEmail(String email);
+    List<User> findAllByActiveTrue();
 }
