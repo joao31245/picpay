632ffc576e8cf4813d6f2760eef8f4d95ce38293
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-11-18 02:11:28 -0300

    Feat: Create an Authentication controller

---
+++ src/main/java/com/jotace/picpay/controller/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.jotace.picpay.controller;
+
+import com.jotace.picpay.domain.user.User;
+import com.jotace.picpay.dto.user.LoginRequest;
+import com.jotace.picpay.dto.user.LoginResponse;
+import com.jotace.picpay.infra.config.security.TokenService;
+import com.jotace.picpay.repositories.UserRepository;
+import com.jotace.picpay.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        var user = userRepository.findUserByEmail(request.email());
+
+        if(user == null) {
+            return ResponseEntity.badRequest().body(new LoginResponse("Deu certo não"));
+        }
+
+        System.out.println("Authorities" + user.getAuthorities());
+
+        var token = tokenService.generateToken((User) user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
+}
