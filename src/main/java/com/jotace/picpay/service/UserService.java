dc9cf45d272772407a6cb023fb8ea748154c3b32
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-11-17 23:16:33 -0300

    Feat: Add basic Crud.

--- src/main/java/com/jotace/picpay/service/UserService.java
+++ src/main/java/com/jotace/picpay/service/UserService.java
@@ -2,6 +2,9 @@ package com.jotace.picpay.service;
 
 import com.jotace.picpay.domain.user.User;
 import com.jotace.picpay.domain.user.UserType;
+import com.jotace.picpay.dto.user.DeleteRequest;
+import com.jotace.picpay.dto.user.UpdateRequest;
+import com.jotace.picpay.dto.user.UserResponse;
 import com.jotace.picpay.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,7 +35,32 @@ public class UserService {
     }
 
     public List<User> getAllUsers() {
-        return repository.findAll();
+        return repository.findAllByActiveTrue();
+    }
+    public User delete(DeleteRequest request) {
+        var user = this.findUserById(request.id());
+
+        user.delete();
+        user.setEmail(user.getEmail() + ".OLD");
+        repository.save(user);
+        return user;
+    }
+
+    public UserResponse update(UpdateRequest request) {
+        var user = this.findUserById(request.id());
+
+        if(request.email().isBlank()|| request.email().equals(user.getEmail())) {
+            throw new RuntimeException("Email is null or equal the older");
+        }
+        if(request.password().isBlank() || request.password().equals(user.getEmail())) {
+            throw new RuntimeException("Password is null or equal the older");
+        }
+
+        user.update(request.email(), request.password());
+
+        save(user);
+
+        return new UserResponse(user);
     }
 
 }
